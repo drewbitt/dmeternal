68ad612f91d76be05aee5e89f6e77b36e731b96c
mattalbert1 <albertm1@nku.edu>
2017-10-12 14:39:47 -0400

    Changes to travis ci New command: mod_stats - Modifies a user specified stat to a specified value for the characters in the campaign's party.

--- game/consolecmd.py
+++ game/consolecmd.py
@@ -19,32 +19,75 @@ def console_func(console, match):
 		console.output(out)
 
 def reset_pc_health(party):
-    '''\
+	'''\
 		Restore the health of all party members
 		Arguments:
 		   party -- a reference to the party of the current campaign
 			 |- reset_health(party) - Example of how to call command
 	'''
-    for pc in party:
-        pc.hp_damage = 0
+	for pc in party:
+		pc.hp_damage = 0
 
 def reset_pc_mana(party):
-    '''\
+	'''\
 		Restore the mana of all party members
 		Arguments:
 		   party -- a reference to the party of the current campaign
 			 |- reset_mana(party) - Example of how to call command
 	'''
-    for pc in party:
-        pc.mp_damage = 0
+	for pc in party:
+		pc.mp_damage = 0
+
+def mod_stat_trigger(party, primaryStat, value):
+	'''\
+		Modify a stat of party characters to be a specified value
+		Arguments:
+			party -- a reference to the party of the current campaign
+			primaryStat -- the Primary Stat that you want to modify. This stat must be typed as a string with all capital charatcers.
+			 | - List of Primary Stats you can modify:
+			 | - STRENGTH
+			 | - TOUGHNESS
+			 | - REFLEXES
+			 | - INTELLIGENCE
+			 | - PIETY
+			 | - CHARISMA
+			value -- the value you want the strength stat to become
+			 | - mod_stat(party, "STRENGTH", 30) - Example of how to call command
+	'''
+	if (primaryStat == "STRENGTH"):
+		for pc in party:
+			pc.statline[stats.STRENGTH] = value
+
+	elif (primaryStat == "TOUGHNESS"):
+		for pc in party:
+			pc.statline[stats.TOUGHNESS] = value
+
+	elif (primaryStat == "REFLEXES"):
+		for pc in party:
+			pc.statline[stats.REFLEXES] = value
+
+	elif (primaryStat == "INTELLIGENCE"):
+		for pc in party:
+			pc.statline[stats.INTELLIGENCE] = value
+
+	elif (primaryStat == "PIETY"):
+		for pc in party:
+			pc.statline[stats.PIETY] = value
+
+	elif (primaryStat == "CHARISMA"):
+		for pc in party:
+			pc.statline[stats.CHARISMA] = value
+
+	else:
+		print("Invalid primary stat")
 
 def super_stats_trigger(party):
-    '''\
+	'''\
 		Modifies the stats of all player characters to be equal to 99
 		Arguments:
 		   party -- a reference to the party of the current campaign
 			 |- super_stats(party) - Example of how to call command
 	'''
-    for pc in party:
-        for stat in stats.PRIMARY_STATS:
-            pc.statline[ stat ] = 99
+	for pc in party:
+		for stat in stats.PRIMARY_STATS:
+			pc.statline[ stat ] = 99
