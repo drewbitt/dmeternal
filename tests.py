df1fd86a56b2289d43b3b12288ec4754dcb19d75
mattalbert1 <albertm1@nku.edu>
2017-11-06 20:16:48 -0500

    Fixing issues with file references (2)

--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 import unittest
-from __init__ import main
+import game
+#from __init__ import main
 #import __init__
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertRaises(Exception, main())
+        self.assertRaises(Exception, game.main())
 
 if __name__ == '__main__':
     #game = main()
