d2bafaa4076f84e801fce273b52944c9ecf0c3a3
Andrew <andrew@drewbitt.com>
2017-11-09 03:12:34 -0500

    travis ci test - advanced

--- test_run.py
+++ test_run.py
@@ -6,5 +6,6 @@ time.sleep(2)
 # tests exiting game
 # press down 5 times to get to exit, then enter to exit
 pyautogui.press(['down', 'down', 'down', 'down', 'down'])
+time.sleep(2)
 pyautogui.press('enter')
 
