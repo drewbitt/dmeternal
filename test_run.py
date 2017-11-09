e195b1d84b59279131064ca618490ebd91dc7484
Andrew <andrew@drewbitt.com>
2017-11-09 04:13:07 -0500

    travis ci test - xvfb test

--- test_run.py
+++ test_run.py
@@ -1,11 +1,15 @@
 import pyautogui
 import time
+import subprocess
 
+subprocess.call(["fg"])
+print("got here after fg!")
 time.sleep(2)
 
 # tests exiting game
 # press down 5 times to get to exit, then enter to exit
 pyautogui.press(['down', 'down', 'down', 'down', 'down'])
+print("got here!")
 time.sleep(2)
 pyautogui.press('enter')
 
